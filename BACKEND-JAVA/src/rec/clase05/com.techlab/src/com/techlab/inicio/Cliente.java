eb04741e9261c95d012d704ea6a60f1f6eaec6e0
dmydna <danimaydana9@gmail.com>
2025-10-11 17:23:15 -0300

    agrega recursos java

---
+++ BACKEND-JAVA/src/rec/clase05/com.techlab/src/com/techlab/inicio/Cliente.java
@@ -0,0 +1,12 @@
+package com.techlab.inicio;
+
+public class Cliente {
+    public String nombre;
+    public String email;
+
+    public  Cliente(String nombre, String email){
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+}
